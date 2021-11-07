5508a453eaece27b5d7748d59038d6e230bd76e1
Cinek2137XD <s25667@pjwstk.edu.pl>
2021-11-07 18:29:45 +0100

    master

--- echo.cpp
+++ echo.cpp
@@ -1,25 +1,103 @@
 #include <iostream>
 #include <string>
 
-auto drukowane(std::string cotam) -> int
+auto drukowane(std::string cotam, int opcja) -> int
 {
+	
+	switch (opcja)
+	{
+		
+		case 1:  //normalne drukowanie
+			std::cout << cotam << " "; 
+		std::cout << "\n";
+		break;
 
+		case 2: //bez endl
+			std::cout << cotam << " "; 
+		break;
 
-	std::cout << cotam;
+	}
+
+	return 0;
 }
+
+
+
 
 auto main(int argc, char* argv[]) -> int
 {
-	if (argc == 0)
+	std::string no_endl,reverse,single;
+	no_endl = "-n";
+	reverse = "-r";
+	single = "-l";
+	if (argc == 1)
 	{
-		return 1;
+		return 0;
 	}
 	else
 	{
-		if(argv[1] == "-r" || argv[2] == "-r" || argv[3] == "-r")
+		switch(argc)
 		{
-			std::cout << argv[3] << " " << argv[2] << " "<< argv[3];
-		} 
+			case 2:
+				if (no_endl == argv[1])
+				{
+					drukowane(no_endl, 2);
+				}
+				else if (reverse == argv[1])
+				{
+					drukowane(reverse, 1);
+				}
+				else
+				{
+					drukowane(single, 1);
+				}
+
+				break;
+			case 3:
+				if (no_endl == argv[1])
+				{
+					drukowane(argv[1], 2);
+					drukowane(argv[2], 2);
+				}
+				else if (reverse == argv[1])
+				{
+					drukowane(argv[2], 2);
+					drukowane(argv[1], 1);
+				}
+				else
+				{
+					drukowane(argv[1], 1);
+					drukowane(argv[2], 1);
+				}
+				break;
+			case 4:
+				if (no_endl == argv[1])
+				{
+					drukowane(argv[1], 2);
+					drukowane(argv[2], 2);
+					drukowane(argv[3], 2);
+				}
+				else if (reverse == argv[1])
+				{
+					drukowane(argv[3], 2);
+					drukowane(argv[2], 2);
+					drukowane(argv[1], 1);
+				}
+				else
+				{
+					drukowane(argv[1], 1);
+					drukowane(argv[2], 1);
+					drukowane(argv[2], 1);
+				}
+				break;	
+		}
+
+		return 0;
 	}
 
 }
+
+
+
+
+/// 
--- piwo.cpp
+++ piwo.cpp
@@ -1,34 +1,57 @@
 #include <iostream>
 #include <string>
 
+auto pytanie(std::string tresc) -> int
+{
+    int a;
+    std::cout << tresc << ": ";
+    std::cin >> a;
+    return a;
+}
+
 auto tekst(int a) -> int
 {
 
-	std::cout << a << " bottles of beer on the wall, "<< a <<"bottles of beer.\n";
+	std::cout << a << " bottles of beer on the wall, "<< a <<" bottles of beer.\n";
 	return 0;
 }
-auto tekst_min(int a) -> int
+auto tekst_min() -> int
 {
 	std::cout << "Take one down, pass it around, ";
 	return 0;
 }
+
+auto tekst_zero() -> int
+{
+	std::cout << "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...\n";
+	return 0;
+}
 // dodać drukowanie 0 cyferki
 
-auto main(int cyferka) -> int
+auto main(int argc, char *argv[]) -> int
 {
-	if(cyferka == 0)
+	int cyferka;
+	
+	//cyferka = pytanie("Podaj cyfre");
+	if(argc == 1)
 	{
-		cyferka = 99
+		cyferka = 99;
 	}
-	for(cyferka >= 0; cyferka--)
+	else
+	{
+		cyferka = std::stoi(argv[1]);
+	}
+	
+	for(cyferka >= 0; cyferka--;)
 	{
 		if(cyferka >0)
 		{
 			tekst(cyferka);
-			text_min();
+			tekst_min();
 		}
 		else if(cyferka == 0)
 		{
+			tekst_zero();
 			//dodać co się dzieje gdy cyferka 0
 		}
 	}

c54f6121feef69552686781284356815815828d1
Cinek2137XD <s25667@pjwstk.edu.pl>
2021-11-22 23:16:32 +0100

    22.11.2021

--- 05-print-thread-group.cpp
+++ 05-print-thread-group.cpp
@@ -4,14 +4,14 @@
 
 void write(int grupa, int watek)
 {
-    std::cout << "Hello, " << tekst << "!\n";
+    std::cout << "Hello, " << grupa<<":"<<watek << "!\n";
 } 
 int main()
 {
     std::thread A;
-    for (int i = 1; i =< 7; i++)
+    for (int i = 1; i <= 7; i++)
     {
-        for (int x = 1; x =< 6; x++)
+        for (int x = 1; x <= 6; x++)
         {
             A = std::thread{write, i,x};
             if (x == 6)
@@ -24,4 +24,5 @@ int main()
             }
         }
     }
+    return 0;
 }
